478898d3dcd0459f2c110af83f91e2d85a8cc6ab
0x4D2 <94604147+0x4D2@users.noreply.github.com>
2022-01-13 09:48:42 +0100

    Admin

--- Forum/admin.php
+++ Forum/admin.php
@@ -5,7 +5,7 @@ require './nav/nav.php';
 
 if (isset($_SESSION['userID'])) {
 
-	$query = " Select benName, f_beGrupID from tblBenutzer inner join tblBenutzergruppe on(f_beGrupID=p_beGrupID) Where beGrupName='Admin' AND p_benID='" . $_SESSION['userID'] . "'";
+	$query = " Select benName, f_beGrupID from tblBenutzer inner join tblBenutzergruppe on(f_beGrupID=p_beGrupID) Where beGrupName='Administator' AND p_benID='" . $_SESSION['userID'] . "'";
 
 	try {
 
@@ -20,6 +20,11 @@ if (isset($_SESSION['userID'])) {
 	if (empty($erg)) {
 		echo "Keine Adminrechte";
 	} else {
+
+		$query = " Select * from tblBenutzer ";
+
+
+
 		echo "Hallo Admin";
 	}
 } else {
