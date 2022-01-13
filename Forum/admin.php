ad04da8fc39052beea27b4f4d86b07c8057c49b5
0x4D2 <94604147+0x4D2@users.noreply.github.com>
2022-01-13 08:35:58 +0100

    Banned
    check if is user Banned

--- Forum/admin.php
+++ Forum/admin.php
@@ -3,36 +3,31 @@ require_once 'verbindung.php';
 require './nav/nav.php';
 
 
-if(isset($_SESSION['userID']) ){
+if (isset($_SESSION['userID'])) {
 
 
 
- $query=" Select benName, f_beGrupID from tblBenutzer inner join tblBenutzergruppe on(f_beGrupID=p_beGrupID) Where beGrupName='Admin' AND p_benID='".$_SESSION['userID']."'";
+	$query = " Select benName, f_beGrupID from tblBenutzer inner join tblBenutzergruppe on(f_beGrupID=p_beGrupID) Where beGrupName='Admin' AND p_benID='" . $_SESSION['userID'] . "'";
 
 
-try{
+	try {
 
-$stmt=$conn->prepare($query);
-$stmt->execute();
-$erg=$stmt->fetch();
-}catch(PDOException $e){
+		$stmt = $conn->prepare($query);
+		$stmt->execute();
+		$erg = $stmt->fetch();
+	} catch (PDOException $e) {
 
-	die("Fehler");
+		die("Fehler");
+	}
+
+	if (empty($erg)) {
+		echo "Keine Adminrechte";
+	} else {
+		echo "Hallo Admin";
+	}
+} else {
+
+	echo 'Sie sind nicht eingeloggt';
+	echo '<a href=login.php>Zum Login....</a>';
+	exit();
 }
-
-if(empty($erg)){
-	echo "Keine Adminrechte";
-}else{
-	echo "Hallo Admin";
-}
-
-
-}else{
-
-echo 'Sie sind nicht eingeloggt';
-echo '<a href=login.php>Zum Login....</a>';
-exit();
-
-}
-
-?>
