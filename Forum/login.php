433476ff234d30c3dd39484446d0e07c29f1fa9f
0x4D2 <94604147+0x4D2@users.noreply.github.com>
2022-01-13 08:59:25 +0100

    Update login.php

--- Forum/login.php
+++ Forum/login.php
@@ -3,47 +3,49 @@
 require_once 'verbindung.php';
 require './nav/nav.php';
 
-if (isset($_POST['ben']) && isset($_POST['passwort'])) {
+if (isset($_SESSION['userID'])) {
+	echo "Sie sind bereits eingeloggt";
+	exit();
+} else {
+
+	if (isset($_POST['ben']) && isset($_POST['passwort'])) {
 
 
-	$query = "SELECT p_benID FROM tblBenutzer
+		$query = "SELECT p_benID FROM tblBenutzer
 				  						WHERE benName = '" . $_POST['ben'] . "'
 				  						AND benPassWd = '" . $_POST['passwort'] . "'";
-										
-										
-	$query2 = "SELECT benIsBanned FROM tblBenutzer WHERE benName = '" . $_POST['ben'] . "'";
+
+		$query2 = "SELECT benIsBanned FROM tblBenutzer WHERE benName = '" . $_POST['ben'] . "'";
 
 
-	try {
-		$stmt = $conn->prepare($query);
-		$stmt->execute();
-		
-		$stmt2 = $conn->prepare($query2);
-		$stmt2->execute();
-		
-		
-	} catch (PDOException $e) {
-		header("Refresh:3;login.php");
-		echo "DatenbankFehler: der Benutzer konnte nicht angelegt werden! <br>";
-		echo $e->getMessage();
-		die("-ENDE_");
-	}
+		try {
+			$stmt = $conn->prepare($query);
+			$stmt->execute();
 
-	$check = $stmt->fetch();
-	$check2 = $stmt2->fetch();
-	if (empty($check) && $check2['benIsBanned'] = 1 ) {
-		echo "Benutzedaten sind falsch oder Sie sind gebannt";
-	} else {
-		header("Refresh:3;index.php");
-		echo "Login Erfolgreich";
-		echo " Weiterleitung";
-		$_SESSION['userID'] = $check['p_benID'];
+			$stmt2 = $conn->prepare($query2);
+			$stmt2->execute();
+		} catch (PDOException $e) {
+			header("Refresh:3;login.php");
+			echo "DatenbankFehler: der Benutzer konnte nicht angelegt werden! <br>";
+			echo $e->getMessage();
+			die("-ENDE_");
+		}
+
+		$check = $stmt->fetch();
+		$check2 = $stmt2->fetch();
+		if (empty($check) && $check2['benIsBanned'] = 1) {
+			echo "Benutzedaten sind falsch oder Sie sind gebannt";
+		} else {
+			header("Refresh:3;index.php");
+			echo "Login Erfolgreich";
+			echo " Weiterleitung";
+			$_SESSION['userID'] = $check['p_benID'];
 
 
-		exit();
+			exit();
+		}
 	}
 }
-
 
 ?>
 
