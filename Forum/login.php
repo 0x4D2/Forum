21340b731b14c55688585add048befe4baa13c9f
0x4D2 <94604147+0x4D2@users.noreply.github.com>
2022-01-20 13:45:30 +0100

    Update login.php

--- Forum/login.php
+++ Forum/login.php
@@ -3,6 +3,7 @@
 require_once 'verbindung.php';
 require './nav/nav.php';
 
+<<<<<<< Updated upstream
 #Hier wird überprüft ob der schon Seassion gesetzt wurde um eine dopplte Anmeldung nich möglich zu machen
 if (isset($_SESSION['userID'])) {
 	echo "Sie sind bereits eingeloggt";
@@ -17,6 +18,22 @@ if (isset($_SESSION['userID'])) {
 		$query2 = "SELECT benIsBanned FROM tblBenutzer WHERE benName = '" . $_POST['ben'] . "'";
 		#Hier werden die SQL-Stamtments vorbereitet ob es den User mit den dazugehören Passwort gibt
 		#Zusätzlich wird überprüft on der User gebannt is
+=======
+if (isset($_SESSION['userID'])) {
+	echo "Sie sind bereits eingeloggt";
+	exit();
+} else {
+
+	if (isset($_POST['ben']) && isset($_POST['passwort'])) {
+
+
+		$query = "SELECT p_benID FROM tblBenutzer
+				  						WHERE benName = '" . $_POST['ben'] . "'
+				  						AND benPassWd = '" . $_POST['passwort'] . "'";
+
+
+		$query2 = "SELECT benIsBanned FROM tblBenutzer WHERE benName = '" . $_POST['ben'] . "'";
+>>>>>>> Stashed changes
 
 
 		try {
@@ -25,10 +42,16 @@ if (isset($_SESSION['userID'])) {
 
 			$stmt2 = $conn->prepare($query2);
 			$stmt2->execute();
+<<<<<<< Updated upstream
 			
 		} catch (PDOException $e) {
 			header("Refresh:3;login.php");
 			#Bein Fehlerhafte Daten wird die login neu gelanden, damit man es erneut versuchen kann.
+=======
+		} catch (PDOException $e) {
+			header("Refresh:3;login.php");
+			echo "DatenbankFehler: der Benutzer konnte nicht angelegt werden! <br>";
+>>>>>>> Stashed changes
 			echo $e->getMessage();
 			die("-ENDE_");
 		}
@@ -37,13 +60,19 @@ if (isset($_SESSION['userID'])) {
 		$check2 = $stmt2->fetch();
 		if (empty($check) && $check2['benIsBanned'] = 1) {
 			echo "Benutzedaten sind falsch oder Sie sind gebannt";
+<<<<<<< Updated upstream
 			#Überprüfung des Staments oder ob User gebannt ist
+=======
+>>>>>>> Stashed changes
 		} else {
 			header("Refresh:3;index.php");
 			echo "Login Erfolgreich";
 			echo " Weiterleitung";
 			$_SESSION['userID'] = $check['p_benID'];
+<<<<<<< Updated upstream
 			#Seession Cookie wird gesetzt 
+=======
+>>>>>>> Stashed changes
 
 
 			exit();
