b64e56d2eef795f43e103d7c5c0fb862acd5b83b
0x4D2 <94604147+0x4D2@users.noreply.github.com>
2022-01-20 13:51:10 +0100

    Update login.php

--- Forum/login.php
+++ Forum/login.php
@@ -3,7 +3,6 @@
 require_once 'verbindung.php';
 require './nav/nav.php';
 
-<<<<<<< Updated upstream
 #Hier wird überprüft ob der schon Seassion gesetzt wurde um eine dopplte Anmeldung nich möglich zu machen
 if (isset($_SESSION['userID'])) {
 	echo "Sie sind bereits eingeloggt";
@@ -18,22 +17,6 @@ if (isset($_SESSION['userID'])) {
 		$query2 = "SELECT benIsBanned FROM tblBenutzer WHERE benName = '" . $_POST['ben'] . "'";
 		#Hier werden die SQL-Stamtments vorbereitet ob es den User mit den dazugehören Passwort gibt
 		#Zusätzlich wird überprüft on der User gebannt is
-=======
-if (isset($_SESSION['userID'])) {
-	echo "Sie sind bereits eingeloggt";
-	exit();
-} else {
-
-	if (isset($_POST['ben']) && isset($_POST['passwort'])) {
-
-
-		$query = "SELECT p_benID FROM tblBenutzer
-				  						WHERE benName = '" . $_POST['ben'] . "'
-				  						AND benPassWd = '" . $_POST['passwort'] . "'";
-
-
-		$query2 = "SELECT benIsBanned FROM tblBenutzer WHERE benName = '" . $_POST['ben'] . "'";
->>>>>>> Stashed changes
 
 
 		try {
@@ -42,28 +25,13 @@ if (isset($_SESSION['userID'])) {
 
 			$stmt2 = $conn->prepare($query2);
 			$stmt2->execute();
-<<<<<<< Updated upstream
-			
 		} catch (PDOException $e) {
 			header("Refresh:3;login.php");
 			#Bein Fehlerhafte Daten wird die login neu gelanden, damit man es erneut versuchen kann.
-=======
-		} catch (PDOException $e) {
-			header("Refresh:3;login.php");
-			echo "DatenbankFehler: der Benutzer konnte nicht angelegt werden! <br>";
->>>>>>> Stashed changes
 			echo $e->getMessage();
-			die("-ENDE_");
-		}
-
-		$check = $stmt->fetch();
-		$check2 = $stmt2->fetch();
 		if (empty($check) && $check2['benIsBanned'] = 1) {
 			echo "Benutzedaten sind falsch oder Sie sind gebannt";
-<<<<<<< Updated upstream
 			#Überprüfung des Staments oder ob User gebannt ist
-=======
->>>>>>> Stashed changes
 		} else {
 			header("Refresh:3;index.php");
 			echo "Login Erfolgreich";
